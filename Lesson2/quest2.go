e11fe5279efaac04292baca67038bc00060b2ee8
Manifoldz <56.zabolotnyi@gmail.com>
2024-03-02 23:44:41 +0500

    Q2final

--- Lesson2/quest2.go
+++ Lesson2/quest2.go
@@ -1,29 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var scoreF1, scoreF2, scoreS1, scoreS2 int8
 	var is1atHome int8 //1-Yes, 2-No
-	if _, err := fmt.Scanf("%d:%d\n%d:%d\n%d", &scoreF1, &scoreF2, &scoreS1, &scoreS2, &is1atHome); err != nil {
+	if _, err := fmt.Scanf("%d:%d\n%d:%d\n%d\n", &scoreF1, &scoreF2, &scoreS1, &scoreS2, &is1atHome); err != nil {
 		fmt.Print("Read-mistake")
 		return
 	} else if !isValidScore(scoreF1) || !isValidScore(scoreF2) || !isValidScore(scoreS1) || !isValidScore(scoreS2) || (is1atHome != 1 && is1atHome != 2) {
 		fmt.Print("Not valid input")
 		return
 	}
+	diff := scoreF2 + scoreS2 - scoreF1 - scoreS1
 	//если итак выигрывает нужно 0
 	if scoreF1+scoreS1 > scoreF2+scoreS2 {
 		fmt.Print(0)
 		return
 	}
 	// если в гостях забила больше, главное сравнять счет
-	if (scoreF1 > scoreS2 && is1atHome == 2) || (scoreS1 > scoreF2 && is1atHome == 1) {
-		fmt.Print(scoreF2 + scoreS2 - scoreF1 - scoreS1)
+	if (scoreF1 > scoreS2 && is1atHome == 2) || (diff+scoreS1 > scoreF2 && is1atHome == 1) {
+		fmt.Print(diff)
 		return
 	}
 
-	fmt.Print(scoreF2 + scoreS2 - scoreF1 - scoreS1 + 1)
+	fmt.Print(diff + 1)
 	return
 
 }
