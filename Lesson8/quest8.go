b1bbc0ffc006627cd7509cb0c9cce18cf05c72a9
Manifoldz <56.zabolotnyi@gmail.com>
2024-03-07 13:04:21 +0500

    Q8done

--- Lesson8/quest8.go
+++ Lesson8/quest8.go
@@ -8,34 +8,48 @@ import (
 func main() {
 	var l, x1, v1, x2, v2 int64
 	_, err := fmt.Scanf("%d %d %d %d %d", &l, &x1, &v1, &x2, &v2)
-	if err != nil || l < 1 || l > 1000000000 || v1 > 1000000000 || v2 > 1000000000 || x1 < 0 || x2 < 0 || x1 >= l || x2 >= l || v1 < 0 || v2 < 0 {
-		fmt.Print("Read-mistake: ", err)
-		return
-	}
-
 	var t float64
 	X1 := float64(x1)
 	X2 := float64(x2)
 	V1 := float64(v1)
 	V2 := float64(v2)
 	L := float64(l)
-	for t <= 100 {
-		if ((X1+V1*t)*360/L)-float64(int64(((X1+V1*t)*360/L)/360)*360) == ((X2+V2*t)*360/L)-float64(int64(((X2+V2*t)*360/L)/360)*360) {
-			fmt.Print(t)
-			break
+
+	if err != nil || l < 1 || l > 1000000000 || int64(math.Abs(V1)) > 1000000000 || int64(math.Abs(V2)) > 1000000000 || x1 < 0 || x2 < 0 || x1 >= l || x2 >= l {
+		fmt.Print("Read-mistake: ", err)
+		return
+	}
+	if x1 == x2 {
+		t = 0
+	} else if V1 == 0 && V2 == 0 { //точки неподвижны в разных местах и исхода нет
+		fmt.Print("NO")
+		return
+	} else if (V1 == 0 && X1 < X2) || (V2 == 0 && X1 > X2) || V1 == V2 || (V1 < 0 && V2 < 0) { //если точки с одинаковой скоростью движутся или одна из них неподвижна
+		//L-X1-V1t = X2+V2t
+		if V1 < 0 && V2 < 0 { // особый случай - в обратку
+			t = (-X1 - X2) / (V2 + V1)
+		} else { //из замеченного условия что всегда в сумме суперпозиций получается сумма круга
+			t = (L - (X1 + X2)) / (V2 + V1)
 		}
-		if math.Floor(((X1+V1*t)*360/L)+((X2+V2*t)*360/L)) == ((X1+V1*t)*360/L)+((X2+V2*t)*360/L) {
-			if int64(((X1+V1*t)*360/L)+((X2+V2*t)*360/L))%360 == 0 {
-				fmt.Print(t)
-				break
+	} else {
+		if X2 > X1 && V1 <= 0 && V2 > 0 { // в обратку через длинную сторону
+			t = (L - X2 + X1) / (V1 - V2)
+		} else if X1 > X2 && V2 <= 0 && V1 > 0 {
+			t = (L - X1 + X2) / (V1 - V2)
+		} else { //стандартный случай
+			if ((L-X1 < X1-X2) || L-X2 < X2-X1) && (V1 >= 0 && V2 >= 0 || V1 <= 0 && V2 <= 0) {
+				t = (L - X1 - X2) / (V1 - V2)
+				temp := float64((L - (X1 + X2 - float64((x1+x2)/l*l))) / (V2 + V1))
+				if temp < math.Abs(t) {
+					t = temp
+				}
+				//fmt.Print("\n", temp < t, "\n")
+			} else {
+				t = (X2 - X1) / (V1 - V2)
 			}
 		}
-		t += 0.0000000001
 	}
 
-	if t <= 100 {
-		fmt.Print("YES\n", t)
-	} else {
-		fmt.Print("NO")
-	}
+	fmt.Print("YES\n", math.Abs(t))
+
 }
